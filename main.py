84498a96c581fbb67a4ef306157d6c7585477dd4
hal <hal@ellingsoncorp.com>
2018-07-19 00:36:02 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import requests
+import S3
+import os
+
+bucket = os.getenv('backup_bucket')
+key = os.getenv('aws_access_key')
+secret = os.getenv('aws_secret')
+
+def check_creds():
+    pass
+
+def upload_backup():
+    pass
+
+def watcher():
+    pass
+
+def preprocess():
+    pass
